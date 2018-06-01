642a55f6b2b79b17055795f100d5516640b30dd7
Sharmina Akhtar <sakht318@gmail.com>
2018-06-01 03:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function findMatching (driver, string) {
     return dName.toLowerCase() === string.toLowerCase();
   });
 }
+
+function fuzzyMatch (driver, string) {
+  return driver.filter(function (dName) {
+    return dName.slice(0, string.length) === dName;
+}
